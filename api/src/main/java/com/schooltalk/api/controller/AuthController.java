7c34811dc2873979f5bb7a4a383d5e00cab85d69
공수정 <brightelf9@naver.com>
2025-04-12 23:03:35 +0900

    refactor: 로그아웃시 Authentication 에서 JWT를 조회해 블랙리스트에 등록

--- api/src/main/java/com/schooltalk/api/controller/AuthController.java
+++ api/src/main/java/com/schooltalk/api/controller/AuthController.java
@@ -1,17 +1,14 @@
 package com.schooltalk.api.controller;
 
-import static com.schooltalk.api.filter.JwtAuthenticationFilter.JWT_AUTH_HEADER;
-import static com.schooltalk.api.filter.JwtAuthenticationFilter.JWT_PREFIX;
-
 import com.schooltalk.api.constants.UrlPath.Auth;
 import com.schooltalk.api.dto.controller.LoginRequest;
 import com.schooltalk.api.dto.controller.LoginResponse;
 import com.schooltalk.api.service.AuthService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,14 +43,13 @@ public class AuthController {
 
 	/**
 	 * 로그아웃
-	 *
-	 * @param token JWT
+	 * @param authentication 인증정보
 	 */
 	@PostMapping(Auth.LOGOUT)
 	@ResponseBody
-	public ResponseEntity<?> logout(@RequestHeader(JWT_AUTH_HEADER) String token) {
-		log.info("Logout request: {}", token);
-		authService.logout(token.replace(JWT_PREFIX, "").trim());
+	public ResponseEntity<?> logout(Authentication authentication) {
+		log.info("Logout request: {}", authentication.getCredentials());
+		authService.logout((String) authentication.getCredentials());
 		return ResponseEntity.ok(null);
 	}
 }
